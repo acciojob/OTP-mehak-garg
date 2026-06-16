e1e7f67cffc280c8dd567060024891590b5b35c2
mehak-garg <mehakgarg741@gmail.com>
2026-06-16 18:17:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,18 @@
 let input=document.querySelectorAll(".code");
 input.forEach((item,i)=>{
 	item.addEventListener("input",()=>{
-		input[i+1].focus();})
+		if (i < input.length - 1) {
+    input[i + 1].focus();
+}})
 						  
 		item.addEventListener("keydown",function(event){
+			
 			if(event.code==="Backspace")
 			{
-				input[i-1].value="";
-                input[i-1].focus();
+				if (i > 0) {
+    input[i].value = "";
+    input[i - 1].focus();
+}
 			}
 		})
 	})
